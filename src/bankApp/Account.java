c38bcf99cab384c7a40ad7c965d7b9ba65012028
Olawale <taofeekolawale159@gmail.com>
2026-07-03 14:53:26 +0100

    Bank App commit

---
+++ src/bankApp/Account.java
@@ -0,0 +1,20 @@
+package bankApp;
+
+public class Account {
+    private int balance;
+    public int getBalance()     {
+        return balance;
+    }
+    public void deposit(int amount) {
+        if (amount > 0) balance = balance + amount;
+    }
+    public void withdraw(int amount) {
+        if (amount > 0 && amount <= balance) balance = balance - amount;
+    }
+
+    public int accountPin(int pin) {
+          pin = 1234;
+        return pin;
+    }
+
+}
---
+++ src/test/AccountTest.java
@@ -0,0 +1,49 @@
+package test;
+
+import bankApp.Account;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AccountTest {
+   private Account shukuratAccount;
+   @BeforeEach
+    public void setUp() {
+      shukuratAccount = new Account();
+    }
+    @Test
+    public void deposit200inEmptyAccount_BalanceIs200(){
+       assertEquals(0, shukuratAccount.getBalance());
+       shukuratAccount.deposit(200);
+       assertEquals(200, shukuratAccount.getBalance());
+    }
+    @Test
+    public void depositMinus50InEmptyAccount_BalanceIsZero(){
+       assertEquals(0, shukuratAccount.getBalance());
+       shukuratAccount.deposit(-50);
+       assertEquals(0, shukuratAccount.getBalance());
+    }
+
+    @Test
+    public void testAccountPin(){
+       int pin = 1234;
+       Account shukuratAccount = new Account();
+     assertEquals(1234, shukuratAccount.accountPin(pin));
+    }
+    @Test
+    public void withdraw50InEmptyAccount_BalanceIsZero(){
+       assertEquals(0, shukuratAccount.getBalance());
+       shukuratAccount.withdraw(50);
+       assertEquals(0, shukuratAccount.getBalance());
+    }
+@Test
+    public void withdraw200InAccount_BalanceIs500(){
+       shukuratAccount.deposit(500);
+       assertEquals(500, shukuratAccount.getBalance());
+       shukuratAccount.withdraw(200);
+       assertEquals(300, shukuratAccount.getBalance());
+
+}
+
+}
